1635c985ca8c6d4461ffb2d6f3a5f4aa85cc66b0
Arun Kashyap <arunkashyap9677@gmail.com>
2023-06-08 16:49:36 +0530

    creating first component

---
+++ src/section/section.js
@@ -0,0 +1,24 @@
+
+
+const page = (
+    <div>
+        <img src="D:/Projects/React/learn-React/react-app/public/logo192.png" width="40px"></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>fact 1</li>
+            <li>fact 2</li>
+            <li>fact 3</li>
+            <li>fact 4</li>
+        </ul>
+    </div>
+);
+
+
+function Section() {
+    return (
+      page
+    );
+  }
+  
+  export default Section;
+  
