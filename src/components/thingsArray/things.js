90b46f6d882a64631ef1de57c72ec44560ba2fe6
Arun Kashyap <arunkashyap9677@gmail.com>
2023-07-19 16:43:37 +0530

    used state

--- src/components/thingsArray/things.js
+++ src/components/thingsArray/things.js
@@ -1,16 +1,26 @@
+import React from "react"
+
 export default function Things() {
-    const things = ["Thing 1", "Thing 2"];
-    const thingElements = things.map(thing => <p>{thing}</p>);
+    const [isGuitarist, setIsGuitarist] = React.useState(true);
+
+    const things = ["Thing 1", "Thing 2", "Thing 3"];
+    const thingElements = things.map(thing => <p key={thing}>{thing}</p>);
 
     function addThing() {
         things.push("thing " + (things.length + 1));
         console.log(things);
     }
 
+    function toggleIsGuitarist() {
+        setIsGuitarist((isGuitarist) => !isGuitarist);
+    }
+
     return (
         <div>
             <button onClick={addThing}>Add Item</button>
             {thingElements}
+            <div>Is he a Guitarist? == <blockquote>{isGuitarist ? "Yes" : "No"}</blockquote></div>
+            <button onClick={toggleIsGuitarist}>Click to change</button>
         </div>
 
     );
